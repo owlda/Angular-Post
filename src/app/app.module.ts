c6c7e344ad260665a1f05bc08bbe877be9b7fa53
owlda <96560023_aD>
2019-11-08 23:43:25 -0800

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostClass } from './posts/create-post/create-post.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { Menu } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostClass,
+    Menu
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
+  ],
+  exports:[
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
